f531e2d88e3d334ede84564fa029c2b09648df46
Artsiom Zhalkouski <azhalkouski@gmail.com>
2023-10-30 21:35:28 +0300

    solve problem from algoexpert 'run-length-encoding'

--- run-length-encoding/run-length-encoding.py
+++ run-length-encoding/run-length-encoding.py
@@ -14,3 +14,10 @@ def runLengthEncoding(string):
         count += 1
     runLength.append(str(count) + string[-1])
     return ''.join(runLength)
+
+
+print(runLengthEncoding("AAAAAAAAAAAAABBCCCCDD") == "9A4A2B4C2D")
+print(runLengthEncoding("aA") == "1a1A")
+print(runLengthEncoding("1A2BB3CCC4DDDD") == "111A122B133C144D")
+print(runLengthEncoding(" ") == "1 ")
+print(runLengthEncoding(";;;;;;;;;;;;''''''''''''''''''''1233333332222211112222111s") == "9;3;9'9'2'111273524142311s")
