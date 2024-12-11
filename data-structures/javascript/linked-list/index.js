d20a3a37d196b91a07d9d80afd49f3e7b450a33b
Artsiom Zhalkouski <azhalkouski@gmail.com>
2024-12-11 16:07:47 +0300

    implement prepend() method

--- data-structures/javascript/linked-list/index.js
+++ data-structures/javascript/linked-list/index.js
@@ -18,8 +18,20 @@ class LinkedList {
   getSize() {
     return this.size;
   }
+
+  prepend(value) {
+    const node = new Node(value);
+    if (!this.isEmpty()) {
+      node.next = this.head;
+    }
+    this.head = node;
+    this.size++;
+  }
 }
 
 const list = new LinkedList();
 console.log("List is empty: ", list.isEmpty());
 console.log("List size", list.getSize());
+list.prepend(10);
+list.prepend(20);
+list.prepend(30);
