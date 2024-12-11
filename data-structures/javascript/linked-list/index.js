5983ca3642ded1ef800add3a0d860fb28ed04f85
Artsiom Zhalkouski <azhalkouski@gmail.com>
2024-12-11 16:31:36 +0300

    implement append() method

--- data-structures/javascript/linked-list/index.js
+++ data-structures/javascript/linked-list/index.js
@@ -19,12 +19,32 @@ class LinkedList {
     return this.size;
   }
 
+  // O(1) time
   prepend(value) {
+    // add a node to the beginnig of the list
     const node = new Node(value);
     if (!this.isEmpty()) {
       node.next = this.head;
     }
     this.head = node;
+    this.size++;
+  }
+
+  // O(n) time, where n is the size of the list
+  append(value) {
+    // add a node to the end of the list
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      // find end
+      let prev = this.head;
+      while (prev.next) {
+        prev = prev.next;
+      }
+      prev.next = node;
+    }
+
     this.size++;
   }
 
@@ -45,11 +65,20 @@ class LinkedList {
 }
 
 const list = new LinkedList();
+// console.log("List is empty: ", list.isEmpty());
+// console.log("List size", list.getSize());
+// list.print();
+// list.prepend(10);
+// list.print();
+// list.prepend(20);
+// list.prepend(30);
+// list.print();
+
 console.log("List is empty: ", list.isEmpty());
 console.log("List size", list.getSize());
 list.print();
-list.prepend(10);
+list.append(10);
 list.print();
-list.prepend(20);
-list.prepend(30);
+list.append(20);
+list.append(30);
 list.print();
