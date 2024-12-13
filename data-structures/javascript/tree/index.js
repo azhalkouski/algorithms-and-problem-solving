9962908f45321412c11db8b2d5094b495854b60e
Artsiom Zhalkouski <azhalkouski@gmail.com>
2024-12-13 18:15:35 +0300

    implement three techniques of Depth First Search tree traversal

--- data-structures/javascript/tree/index.js
+++ data-structures/javascript/tree/index.js
@@ -58,6 +58,33 @@ class BinarySearchTree {
     }
   }
 
+  // `preorder()` method is one of three ways to implement Depth First Search
+  preorder(root) {
+    if (root) {
+      console.log(root.value);
+      this.preorder(root.left);
+      this.preorder(root.right);
+    }
+  }
+
+  // `inorder()` method is one of three ways to implement Depth First Search
+  inorder(root) {
+    if (root) {
+      this.inorder(root.left);
+      console.log(root.value);
+      this.inorder(root.right);
+    }
+  }
+
+  // `postorder()` method is one of three ways to implement Depth First Search
+  postorder(root) {
+    if (root) {
+      this.postorder(root.left);
+      this.postorder(root.right);
+      console.log(root.value);
+    }
+  }
+
 
 }
 
@@ -69,9 +96,18 @@ console.log("BST is empty: ", bst.isEmpty());
 bst.insert(10);
 bst.insert(5);
 bst.insert(15);
+bst.insert(3);
+bst.insert(7);
 
-console.log(bst.search(bst.root, 10));
-console.log(bst.search(bst.root, 5));
-console.log(bst.search(bst.root, 15));
+bst.preorder(bst.root);
+console.log("");
+bst.inorder(bst.root);
+console.log("");
+bst.postorder(bst.root);
 
-console.log(bst.search(bst.root, 20));
+// console.log(bst.search(bst.root, 10));
+// console.log(bst.search(bst.root, 5));
+// console.log(bst.search(bst.root, 15));
+
+// console.log(bst.search(bst.root, 20));
+
