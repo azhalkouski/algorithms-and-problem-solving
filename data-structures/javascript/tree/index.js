450b827b92d76eacc48dbdcf5bbd4faf0d29c4dc
Artsiom Zhalkouski <azhalkouski@gmail.com>
2024-12-13 17:26:14 +0300

    implement search in BinarySearchTree

--- data-structures/javascript/tree/index.js
+++ data-structures/javascript/tree/index.js
@@ -44,6 +44,20 @@ class BinarySearchTree {
     }
   }
 
+  search(root, value) {
+    if (!root) {
+      return false;
+    } else {
+      if (root.value === value) {
+        return true;
+      } else if (value < root.value) {
+        return this.search(root.left, value);
+      } else {
+        return this.search(root.right, value)
+      }
+    }
+  }
+
 
 }
 
@@ -55,3 +69,9 @@ console.log("BST is empty: ", bst.isEmpty());
 bst.insert(10);
 bst.insert(5);
 bst.insert(15);
+
+console.log(bst.search(bst.root, 10));
+console.log(bst.search(bst.root, 5));
+console.log(bst.search(bst.root, 15));
+
+console.log(bst.search(bst.root, 20));
